8ca64908707c5b95ae943671d3a439c5549826de
MMxM <mmedeiros450@gmail.com>
2015-02-10 15:19:12 -0200

    melhorando o codigo

--- addslashes.c
+++ addslashes.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <assert.h>
+#include <errno.h>
 
 // By MMxM
 // C version of addslashes (PHP)
 
-
 char *addslashes(const char *str);
+void *xmalloc(size_t len);
+void safefree(void **ptr);
+void die(const char *err);
+
+void die(const char *err){
+	(errno) ? perror(err) : fprintf(stderr,"%s\n",err);
+	exit(1);
+}
+
+void *xmalloc(size_t len){
+	void *ptr = malloc(len);
+	if(ptr == NULL)
+		die("malloc error");
+}
+
+void xfree(void **ptr){
+	assert(ptr);
+	if(ptr != NULL){
+		free(*ptr);
+		*ptr = NULL;
+	}
+}
 
 char *addslashes(const char *str){
-	int i = 0,x = 0, j = 0,size = strlen(str),size_x;
-	char *str2;
+	size_t i = 0, x = 0, j = 0, size_x = 0;
+	char *str2 = NULL;
 
-	for(i=0;i<size;i++){
+	for(i=0; str[i]; i++){
 		if(str[i] == 0x5c)
 			x++;
 		else if(str[i] == 0x27)
@@ -22,41 +44,54 @@ char *addslashes(const char *str){
 			x++;
 	}
 
-	size_x = x+i;
+	size_x = x+i+1;
 
-	str2 = (char *)malloc(size_x);
-	memset(str2,0,sizeof(str2));
+	str2 = malloc(size_x*sizeof(char));
 
-	for(i=0;i<size;i++){
+	for(i=0;str[i];i++){
 		char c = str[i];
+
 		if(c == 0x27){
 			str2[j] = 0x5c;
 			j++;
 			str2[j] = c;
 		}
+
 		else if(c == 0x5c){
 			str2[j] = c;
 			j++;
 			str2[j] = c;
 		}
+
 		else if(c == 0x22){
 			str2[j] = 0x5c;
 			j++;
 			str2[j] = c;
 		}
+
 		else {
 			str2[j] = c;
 		}
+
 		j++;
 	}
+
+	str2[j] = 0x0;
 	return str2;
 }
 
-int main(void){
-	const char *test = "I'm X | \"Y\" | C:\\Windows\\System32 ' ' \\' ' ";
-	char *test2 = addslashes(test);
-	printf("Without addslashes: %s\n",test);
-	printf("With addslashes: %s\n",test2);
-	free(test2);
+int main(int argc, char **argv){
+	char *test = NULL;
+
+	if(argc != 2)
+		return 0;
+
+	test = addslashes(argv[1]);
+
+	printf("Without addslashes: %s\n",argv[1]);
+	printf("With addslashes: %s\n",test);
+
+	xfree((void **)&test);
+
 	return 0;
 }
