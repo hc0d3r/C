b9adfa2529b3c8992d4a0cf62206f3c9faad717c
MMxM <mmedeiros450@gmail.com>
2015-03-28 16:20:07 -0300

    melhorando o codigo (/-)/

--- linux-cpu-usage.c
+++ linux-cpu-usage.c
@@ -5,57 +5,67 @@
 
 /* ************************************ *
  * CPU USAGE CALC , Using /proc/stat	*
- * Qui Mai  1 11:26:43 BRT 2014		*
+ * Sáb Mar 28 16:18:44 BRT 2015		*
  * Author: MMxM | hc0der.blogspot.com	*
  * ************************************	*/
 
+
+
 int main(void){
 	FILE *proc;
-	char line[60];
-	int prev_idle = 0,
-	prev_total = 0, idle = 0,diff_usage = 0,
-	diff_idle = 0,diff_total = 0,
-	tmp_p = 0, v_p = 0,sum = 0;
-	char tmp[10];
+
+	char c;
+
+	int i=0, cpu_found=0, tmp = 0, total=0, prev_total=0, diff_idle=0, diff_total=0, idle=0, prev_idle=0, x=0, diff_usage=0;
+
 	while(1){
-		int vet[10] = { 0 },i=0;
-		v_p = 0,sum = 0;
-		proc = fopen("/proc/stat","r");
-		fgets(line,60,proc);
-		int len = strlen(line)-1;
-		line[len] = '\0';
-		while(i!=len-1){
-			if(line[i] >= 48 && line[i] <= 57){
-				while(i){
-					if(line[i] == 32 || line[i] == 10) break;
-					tmp[tmp_p] = line[i];
-					i++;
-					tmp_p++;
-				}
-				tmp[tmp_p] = '\0';
-				vet[v_p] = atoi(tmp);
-				v_p++;
-				tmp_p = 0;
+		if( (proc = fopen("/proc/stat","r")) == NULL){
+			printf("Failed to open file\n");
+			return 1;
+		}
+
+
+		while( (c = fgetc(proc)) != EOF ){
+			if( (c == 'c' && i == 0) || (c == 'p' && i == 1) || (c == 'u' && i == 2)){
+				cpu_found++;
+			}
+
+			else if( c >= '0' && c <= '9' && cpu_found == 3){
+				tmp *= 10;
+				tmp += c-'0';
+			}
+
+			else if( c == ' ' && cpu_found == 3 ){
+				total += tmp;
+				x++;
+				if( x == 6 )
+					idle = tmp;
+				tmp = 0;
+			}
+
+			else if( c == '\n' && cpu_found == 3 ){
+				i = 0;
+				cpu_found = 0;
+				total += tmp;
+				tmp = 0;
+				break;
 			}
 			i++;
 		}
 
-		int ii;
-		for(ii=0;ii<9;ii++)
-			sum += vet[ii];
+		diff_idle = idle-prev_idle;
+		diff_total = total-prev_total;
 
-		idle = vet[3];
-		diff_idle = idle - prev_idle;
-		diff_total = sum - prev_total;
-		diff_usage = 100*(diff_total - diff_idle);
-		prev_idle = idle;
-		prev_total = sum;
+		prev_total=total;
+		prev_idle=idle;
+
+		diff_usage=100*(diff_total - diff_idle);
+
+		printf("CPU: %6.2f%%\r", (float)diff_usage/(float)diff_total );
 		fflush(stdout);
-		fprintf(stdout,"CPU: %6.2f%%\r",(float)diff_usage/(float)diff_total);
-		
-		fclose(proc);
 		sleep(1);
 	}
 
+	return 0;
 
 }
