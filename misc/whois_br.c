3b18974a914682bbee5e43e6621b333e5d0d7a62
Matheus <mmedeiros450@gmail.com>
2019-03-28 04:54:15 -0300

    update whois_br.c

--- misc/whois_br.c
+++ misc/whois_br.c
@@ -1,11 +1,4 @@
-/* ==============================
- * $ date
- * Sáb Mar 28 17:10:06 BRT 2015
- * $ whoami
- * MMxM
- * $ pwd
- * http://hc0der.blogspot.com
- * ============================== */
+// gcc whois_br.c -o whois_br
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -19,60 +12,55 @@
 
 #define WHOIS "registro.br"
 
-void whois_br(const char *hostname);
-void error(char *msg);
-void help(void);
+int whois(char *hostname){
+    struct sockaddr_in addr;
+    struct hostent *host;
+    int fd, nbytes, ret = 1;
+    char buf[1024];
 
-void help(void){
-	fprintf(stderr,"\n[+] Whois query .br domains\n");
-	fprintf(stderr,"[-] *(Coder)-> MMxM\n");
-	fprintf(stderr,"[*] Usage: ./whois_br (domain).br\n");
-	fprintf(stderr,"[*] Example: ./whois_br google.com.br\n\n");
-	exit(1);
+
+    size_t len = strlen(hostname);
+    hostname[len++] = '\n';
+
+    fd = socket(AF_INET,SOCK_STREAM,0);
+
+    if(fd == -1){
+        perror("failed to create socket");
+        goto end;
+    }
+
+    host = gethostbyname(WHOIS);
+
+    addr.sin_family = host->h_addrtype;
+    addr.sin_port   = htons(43);
+    addr.sin_addr   = *((struct in_addr *)host->h_addr);
+
+    if(connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1){
+        perror("connection failed");
+        goto end;
+    }
+
+    if(send(fd, hostname, len, 0) == -1){
+        perror("failed to send hostname");
+        goto end;
+    }
+
+    while((nbytes = recv(fd, buf, sizeof(buf), 0)) > 0){
+        write(STDOUT_FILENO, buf, nbytes);
+    }
+
+    ret = 0;
+
+    end:
+    return ret;
 }
 
-void error(char *msg){
-	perror(msg);
-	exit(1);
-}
+int main(int argc, char **argv){
 
-void whois_br(const char *hostname){
-	int msocket,recebidos;
-	char resposta[5000];
-	struct sockaddr_in addr;
-	struct hostent *host;
+    if(argc != 2){
+        puts("usage: whois_br [domain.br]");
+        return 0;
+    }
 
-	msocket = socket(AF_INET,SOCK_STREAM,0);
-
-	if(msocket == -1)
-		error("Fail to create socket ");
-
-	host = gethostbyname(WHOIS);
-
-	addr.sin_family	= host->h_addrtype;
-	addr.sin_port	= htons(43);
-	addr.sin_addr	= *((struct in_addr *)host->h_addr);
-
-	memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
-
-	if(connect(msocket,(struct sockaddr*)&addr,sizeof(addr)) == -1)
-		error("Fail to connect");
-
-	if( (send(msocket, hostname, strlen(hostname),0) == -1) || (send(msocket, "\n", 1, 0) == -1) )
-		error("Fail to make request");
-
-	while( (recebidos = recv(msocket,resposta,4999,0)) ){
-		resposta[recebidos] = '\0';
-		puts(resposta);
-	}
-}
-
-int main(int argc,char **argv){
-
-	if(argc != 2)
-		help();
-
-	whois_br(argv[1]);
-
-	return 0;
+    return whois(argv[1]);
 }
