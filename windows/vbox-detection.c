0502e6d134e0ace5ef144c1bd434a96c35b6122e
Matheus <mmedeiros450@gmail.com>
2019-03-29 23:21:56 -0300

    update vbox-detection.c

--- windows/vbox-detection.c
+++ windows/vbox-detection.c
@@ -2,43 +2,49 @@
 #include <winreg.h>
 #include <string.h>
 
-/*
- Virtual box detection checking an register key
- c0der => MMxM [http://hc0der.blogspot.com]
-*/
+// Virtual box detection through the windows register
 
-int CheckVbox(void){
+int checkVbox(void){
+    int ret = 0;
 
-	HKEY key;
-	DWORD tipo = REG_MULTI_SZ, size = 150;
-	BYTE value[500];
+    DWORD ktype = REG_MULTI_SZ, size = 128;
+    BYTE value[256];
+    HKEY key;
 
-	if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("HARDWARE\\DESCRIPTION\\System"), 0, KEY_READ, &key) != ERROR_SUCCESS){
-		return 0;
-	}
- 
-	if( RegQueryValueEx(key, TEXT("SystemBiosVersion"), 0, &tipo, value, &size) != ERROR_SUCCESS){
-		return 0;
-	}
- 
-	RegCloseKey(key);
- 
-	if(strstr(value,"VBOX")){
-		return 1;
-	} else {
-		return 0;
-	}
+    if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("HARDWARE\\DESCRIPTION\\System"), 0,
+        KEY_READ, &key) != ERROR_SUCCESS){
+        goto end;
+    }
 
+    if(RegQueryValueEx(key, TEXT("SystemBiosVersion"), 0,
+        &ktype, value, &size) != ERROR_SUCCESS){
+        goto end;
+    }
+
+    RegCloseKey(key);
+
+    if(strstr(value,"VBOX")){
+        ret = 1;
+    }
+
+    end:
+    return ret;
 }
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+    LPSTR msg;
+    UINT opt;
 
-	if(CheckVbox()){
-		MessageBox(NULL, "Virtual box detected !!!", "Virtual Box", MB_ICONEXCLAMATION);
-	} else {
-		MessageBox(NULL, "Virtual box not detected !!!", "Virtual Box", MB_OK);
-	}
+    if(checkVbox()){
+        msg = "Virtual box detected !!!";
+        opt = MB_ICONEXCLAMATION;
+    } else {
+        msg = "Virtual box not detected !!!";
+        opt = MB_OK;
+    }
 
-	return 0;
+    MessageBox(NULL, msg, "Virtual Box", opt);
+
+    return 0;
 }
