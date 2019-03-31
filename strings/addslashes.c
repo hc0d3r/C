7b1fce0ae5da10755c42df29adeda74b566737e7
Matheus <mmedeiros450@gmail.com>
2019-03-31 02:32:35 -0300

    update addslashes.c

--- strings/addslashes.c
+++ strings/addslashes.c
@@ -1,97 +1,67 @@
+// gcc addslashes.c -o addslashes -D_HAVE_MAIN
+// addslashes like PHP
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <errno.h>
 
-// By MMxM
-// C version of addslashes (PHP)
+#define quote(c) (c == '"' || c == '\'' || c == '\\' || c == 0x0)
 
-char *addslashes(const char *str);
-void *xmalloc(size_t len);
-void safefree(void **ptr);
-void die(const char *err);
+char *addslashes(const char *str, size_t size){
+    char *ret, *ptr, c;
+    size_t i, len = 0;
 
-void die(const char *err){
-	(errno) ? perror(err) : fprintf(stderr,"%s\n",err);
-	exit(1);
+    for(i=0; i<size; i++){
+        if(quote(str[i]))
+            len++;
+    }
+
+    len += i+1;
+
+    ret = ptr = malloc(len);
+    if(ret == NULL)
+        goto end;
+
+    i = 0;
+
+    while(i < size){
+        c = str[i++];
+
+        if(quote(c)){
+            *ptr++ = '\\';
+            if(c){
+                *ptr++ = c;
+            } else {
+                *ptr++ = '0';
+            }
+        } else {
+            *ptr++ = c;
+        }
+    }
+
+    *ptr = 0x0;
+
+    end:
+    return ret;
 }
 
-void *xmalloc(size_t len){
-	void *ptr = malloc(len);
-	if(ptr == NULL)
-		die("malloc error");
+#ifdef _HAVE_MAIN
+int main(void){
+    char *test = NULL;
+
+    test = addslashes("\x00\x00\x00\x61", 4);
+    printf("%s\n", test);
+    free(test);
+
+    test = addslashes("'-'", 3);
+    printf("%s\n", test);
+    free(test);
+
+    // +1 for get null byte
+    test = addslashes("\\ \"hello world\" \\", 18);
+    printf("%s\n", test);
+    free(test);
+
+
+    return 0;
 }
-
-void xfree(void **ptr){
-	assert(ptr);
-	if(ptr != NULL){
-		free(*ptr);
-		*ptr = NULL;
-	}
-}
-
-char *addslashes(const char *str){
-	size_t i = 0, x = 0, j = 0, size_x = 0;
-	char *str2 = NULL;
-
-	for(i=0; str[i]; i++){
-		if(str[i] == 0x5c)
-			x++;
-		else if(str[i] == 0x27)
-			x++;
-		else if(str[i] == 0x22)
-			x++;
-	}
-
-	size_x = x+i+1;
-
-	str2 = malloc(size_x*sizeof(char));
-
-	for(i=0;str[i];i++){
-		char c = str[i];
-
-		if(c == 0x27){
-			str2[j] = 0x5c;
-			j++;
-			str2[j] = c;
-		}
-
-		else if(c == 0x5c){
-			str2[j] = c;
-			j++;
-			str2[j] = c;
-		}
-
-		else if(c == 0x22){
-			str2[j] = 0x5c;
-			j++;
-			str2[j] = c;
-		}
-
-		else {
-			str2[j] = c;
-		}
-
-		j++;
-	}
-
-	str2[j] = 0x0;
-	return str2;
-}
-
-int main(int argc, char **argv){
-	char *test = NULL;
-
-	if(argc != 2)
-		return 0;
-
-	test = addslashes(argv[1]);
-
-	printf("Without addslashes: %s\n",argv[1]);
-	printf("With addslashes: %s\n",test);
-
-	xfree((void **)&test);
-
-	return 0;
-}
+#endif
